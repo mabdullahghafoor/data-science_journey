a029544a0f524c126c5410c136cae0a89296f6c3
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-03-25 23:01:23 +0500

    Day 01 Going through sme examples of variables and data types along with their some operations

---
+++ Day_01/str_operations.py
@@ -0,0 +1,26 @@
+# in this code we will ue some string operations
+
+name = " Ali Khan "
+
+print(name.strip())
+print(name.lower())
+print(name.upper())
+print(name.replace("Khan","Ahmad"))
+print(len(name))
+
+
+# Format Strinf
+
+std = "Ali"
+score = 85
+
+print(f"Stude {std} scored {score}%")
+
+# String Indexing
+
+word = "Python"
+
+print(word[0]) # print 1st char
+print(word[-1]) # print last char
+print(word[0:3]) # print chars 0 till 2
+print(word[::2]) # print every 2nd char
---
+++ Day_01/var_&_dt.py
@@ -0,0 +1,17 @@
+# This code is all about variable and data types
+
+# Creating variables
+
+name = "Ali"
+age = 20
+gpa = 3.4
+is_enrolled = True
+grade = None
+
+# Checking Their data types:
+
+print(type(name))
+print(type(age))
+print(type(gpa))
+print(type(is_enrolled))
+print(type(grade))
