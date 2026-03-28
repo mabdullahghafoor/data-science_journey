51f633e6a61678bb72a087b6e51140361acd2081
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-03-28 15:58:50 +0500

    Update match_statement.py

---
+++ Day_03/match_statement.py
@@ -0,0 +1,13 @@
+# In tis we will solve some match statement examples
+
+day = input("Enetr a day number(1-7): ")
+
+match day:
+    case "1": print("Monday")
+    case "2": print("Tuesday")
+    case "3": print("Wednesday")
+    case "4": print("Thursday")
+    case "5": print("Friday")
+    case "6": print("Saturday")
+    case "7": print("Sunday")
+    case _: print("Invalid day")
