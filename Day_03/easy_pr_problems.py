31da059c827980a818b6ea0a954612c36fb178a9
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-03-29 15:37:38 +0500

    Create easy_pr_problems.py

---
+++ Day_03/easy_pr_problems.py
@@ -0,0 +1,12 @@
+# In This program we will solve some easy practice problems for operators
+
+# Q1. Write a program that takes a number and prints whether it is Positive, Negative, or Zero.
+
+num = int(input("Enter a number: "))
+
+if (num > 0):
+    print("Positive")
+elif (num < 0):
+    print("Negative")
+else:
+    print("Zero")
