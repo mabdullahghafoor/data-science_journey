0f0effb467e1568704afbde356a5438e24904d4e
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-04-01 09:54:01 +0500

    Create list_methods.py

---
+++ Day_05/list_methods.py
@@ -0,0 +1,13 @@
+# In this program we will see some list methids
+
+scores = [88, 76, 92, 65, 95, 76, 88, 100, 45]
+
+# information methods
+
+print(len(scores))
+print(scores.count(76))
+print(scores.index(92))
+print(sum(scores))
+print(max(scores))
+print(min(scores))
+
