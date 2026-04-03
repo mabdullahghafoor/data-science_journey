cd729783dc6f6f940acd40a3ecfe20e74c7f626d
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-04-03 16:45:39 +0500

    Update medium_pr_prob.py

--- Day_05/medium_pr_prob.py
+++ Day_05/medium_pr_prob.py
@@ -1,35 +1,23 @@
 # In this we will see some some medium level practice problems of lists.
 
-#Q4. Given a list of marks, write a program that:
+marks = [80, 57, 33, 69, 52, 39]
 
-#Removes all marks below 40 (failed)
-#Sorts remaining in descending order
-#Prints top 3 performers
+# 1️⃣ Remove failed marks
+new_marks = [mark for mark in marks if mark >= 40]
+print("Passed marks:", new_marks)
 
-marks = [80,57,33,69,52,39]
-new_marks = []
+# 2️⃣ Manual bubble sort in descending order
+n = len(new_marks)
+for i in range(n):
+    for j in range(0, n-i-1):
+        if new_marks[j] < new_marks[j+1]:
+            # Swap
+            temp = new_marks[j]
+            new_marks[j] = new_marks[j+1]
+            new_marks[j+1] = temp
 
+print("Sorted marks:", new_marks)
 
-for mark in marks:
-
-    if mark >=  40:
-        new_marks.append(mark)
-
-print(new_marks)
-
-
-sorted_list = []
-temp = 0
-for j in range(len(marks)):
-    for i in range(len(new_marks)-1):
-        if new_marks[i] < new_marks[i+1]:
-            temp = new_marks[i] 
-            new_marks[i] = new_marks[i+1]
-            new_marks[i+1] = temp
-            
-    
-       
-top3 = new_marks[0:3]
-print(top3)
-
-
+# 3️⃣ Top 3 performers
+top3 = new_marks[:3]
+print("Top 3 performers:", top3)
