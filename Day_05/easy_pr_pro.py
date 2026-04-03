55466051f0866bc2656b800a73628df025c8e027
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-04-03 12:04:57 +0500

    First easy level problem

--- Day_05/easy_pr_pro.py
+++ Day_05/easy_pr_pro.py
@@ -1,2 +1,13 @@
 # In this we will see some some easy level practice problems of lists.
 
+# Q1. Create a list of 5 cities in Pakistan. Print:
+
+# First city
+#Last city
+# All cities in reverse order
+
+city = ["karachi", "lahore", "islamabad", "multan", "pindi"]
+
+print(city[0])
+print(city[-1])
+print(city[::-1])
