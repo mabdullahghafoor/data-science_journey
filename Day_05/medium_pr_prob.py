00d73588b5f1d979ba1de0cbf33c221ce4f63dab
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-04-03 16:42:47 +0500

    second medium problem done

--- Day_05/medium_pr_prob.py
+++ Day_05/medium_pr_prob.py
@@ -9,11 +9,27 @@
 marks = [80,57,33,69,52,39]
 new_marks = []
 
+
 for mark in marks:
 
     if mark >=  40:
         new_marks.append(mark)
 
 print(new_marks)
+
+
+sorted_list = []
+temp = 0
+for j in range(len(marks)):
+    for i in range(len(new_marks)-1):
+        if new_marks[i] < new_marks[i+1]:
+            temp = new_marks[i] 
+            new_marks[i] = new_marks[i+1]
+            new_marks[i+1] = temp
+            
+    
+       
+top3 = new_marks[0:3]
+print(top3)
 
 
