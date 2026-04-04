ad65903775695864646bf68211682a78564d69ee
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-04-04 14:13:06 +0500

    Create hard_pr_prob.py

---
+++ Day_05/hard_pr_prob.py
@@ -0,0 +1,11 @@
+# Q7. Build a shopping cart system using lists:
+
+# Add items (name + price)
+# Remove items by name
+# View cart
+# Calculate total
+# Apply 10% discount if total > PKR 2000
+# Print formatted receipt
+
+
+
