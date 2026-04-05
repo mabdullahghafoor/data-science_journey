a77b63dfae688887a910d4e719057cba16b8256f
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-04-05 16:40:59 +0500

    Update creating_&accessing_tuples.py

--- PHASE_ONE/Day_06/creating_&accessing_tuples.py
+++ PHASE_ONE/Day_06/creating_&accessing_tuples.py
@@ -12,3 +12,9 @@ wrong  = (42)       # this is just integer 42 — NOT a tuple
 correct = (42,)     # THIS is a tuple — notice the comma!
 print(type(wrong))   # <class 'int'>
 print(type(correct)) # <class 'tuple'>
+
+
+# ── Accessing — exactly like lists ──────────────────────────────
+print(student_info[0])      # Ali Hassan  → first item
+print(student_info[-1])     # 3.87        → last item
+print(student_info[1:3])    # (22, 'FAST') → slicing works too!
