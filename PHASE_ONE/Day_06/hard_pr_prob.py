7a8cd5a9b69b46dab309ef392f87b247caa8e32e
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-04-05 18:26:46 +0500

    Question listed

--- PHASE_ONE/Day_06/hard_pr_prob.py
+++ PHASE_ONE/Day_06/hard_pr_prob.py
@@ -1,1 +1,14 @@
 # In this program we will solve some medium level practice problems of tuples
+
+
+# Q7. You are given monthly sales data as a tuple 
+# (cannot change — it's historical data):
+                                                 
+# Write a program that prints:
+
+# Best month (name + sales)
+# Worst month (name + sales)
+# Total annual sales
+# Average monthly sales
+# Months that were above average
+
