ec008ae5e8fe77cfd847520f5c1deb199d490dde
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-04-05 18:44:11 +0500

    Question Added

--- PHASE_ONE/Day_06/mini_challenge.py
+++ PHASE_ONE/Day_06/mini_challenge.py
@@ -1,2 +1,11 @@
 # In this program we will perform a mini challenge for tuple
 
+# Create a system using tuples that stores flight records:
+# Print a formatted flight board and answer:
+# - Most expensive flight
+# - Cheapest flight
+# - All flights FROM Karachi
+# - Average ticket price
+
+
+
