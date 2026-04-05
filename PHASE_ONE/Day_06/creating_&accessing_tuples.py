b18310b287c78e2e1cc0c2b5dd4e5e60be842992
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-04-05 16:40:24 +0500

    Look how to access tuples

--- PHASE_ONE/Day_06/creating_&accessing_tuples.py
+++ PHASE_ONE/Day_06/creating_&accessing_tuples.py
@@ -5,3 +5,10 @@ coordinates  = (24.8607, 67.0011)           # Karachi GPS
 rgb_red      = (255, 0, 0)                  # color values
 student_info = ("Ali Hassan", 22, "FAST", 3.87)
 empty_tuple  = ()                           # empty tuple
+
+
+# ⚠️ VERY IMPORTANT — single item tuple needs a trailing comma!
+wrong  = (42)       # this is just integer 42 — NOT a tuple
+correct = (42,)     # THIS is a tuple — notice the comma!
+print(type(wrong))   # <class 'int'>
+print(type(correct)) # <class 'tuple'>
