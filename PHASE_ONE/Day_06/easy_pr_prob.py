0bcd10c59c2b845242a005baf1d242e96f5a61d3
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-04-05 17:00:11 +0500

    Question # 01 Done!

--- PHASE_ONE/Day_06/easy_pr_prob.py
+++ PHASE_ONE/Day_06/easy_pr_prob.py
@@ -1,2 +1,11 @@
-# In this program we will look some easy level practice problems of tuples
+# In this program we will solve some easy level practice problems of tuples
 
+# Q1. Create a tuple storing your: name, age, city, and favourite programming language. 
+# #Print each item using tuple unpacking.
+
+data = ("Abdullah", 21, "Karachi", "Python")
+name, age, city, fav_lang = data
+print(name)
+print(age)
+print(city)
+print(fav_lang)
