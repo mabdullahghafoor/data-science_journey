4d92be46248bdcb5f09a942b03daebf297bbf9c5
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-04-05 17:10:18 +0500

    question  # 01 Done!

--- PHASE_ONE/Day_06/medium_pr_prob.py
+++ PHASE_ONE/Day_06/medium_pr_prob.py
@@ -1,1 +1,17 @@
 # In this program we will solve some medium level practice problems of tuples
+
+# Q4. Try to change an item in a tuple. Observe the error message.
+# Then write code to: #
+# convert to list → change the value → convert back to tuple. 
+# #Print the final tuple.
+
+data = (5,3,8,9,1,6,2)
+# data[0] = 3
+print(data)
+
+# Error : TypeError: 'tuple' object does not support item assignment
+
+data_list = list(data)
+data_list[0] = 10
+data_tuple = tuple(data_list)
+print(data_tuple)
