98a0c84a8babe026b1d036c8fd9e327ed35ea9ff
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-04-06 08:26:24 +0500

    Exploring union operator

--- PHASE_ONE/Day_07/sets_operations.py
+++ PHASE_ONE/Day_07/sets_operations.py
@@ -4,3 +4,10 @@
 python_students = {"Ali", "Sara", "Fatima", "Omar", "Zara"}
 sql_students    = {"Sara", "Omar", "Bilal", "Hina", "Fatima"}
 
+
+# ── UNION ( | ) → ALL students from BOTH groups ─────────────────
+all_students = python_students | sql_students
+print(f"All students      : {all_students}")
+# {'Ali','Sara','Fatima','Omar','Zara','Bilal','Hina'}
+
+
