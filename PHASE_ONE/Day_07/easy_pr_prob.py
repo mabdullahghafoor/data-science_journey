a9905ab34dd4451f0546c15cc3baaa6dff76bc74
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-04-06 08:49:49 +0500

    Question # 01 Added

--- PHASE_ONE/Day_07/easy_pr_prob.py
+++ PHASE_ONE/Day_07/easy_pr_prob.py
@@ -1,2 +1,9 @@
 # In this program we will solve some easy level practice problems of Sets
 
+
+# Q1. Create a list with duplicate values:
+# Convert to set to remove duplicates.
+# Print how many duplicates were removed.
+
+
+
