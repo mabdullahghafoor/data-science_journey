91b4048030046f74744f186374dbad188d7d6a9a
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-04-06 10:42:06 +0500

    Question Added

--- PHASE_ONE/Day_07/hard_pr_prob.py
+++ PHASE_ONE/Day_07/hard_pr_prob.py
@@ -1,1 +1,9 @@
 # In this program we will solve hard level practice problems of Sets
+
+
+#Q7. You are given 4 weeks of website visitor data (user IDs):
+#Find:
+#Visitors who came ALL 4 weeks (loyal users)
+#Visitors who came only ONCE across all weeks
+#Total unique visitors across all 4 weeks
+#New visitors each week (not seen in previous weeks)
