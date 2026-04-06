b6a8c66ed6ca5b7fd096e5285710a8b1a592c244
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-04-06 08:39:49 +0500

    Removing Duplicates From Data

--- PHASE_ONE/Day_07/real_world_example.py
+++ PHASE_ONE/Day_07/real_world_example.py
@@ -6,3 +6,12 @@
 source_A = [101, 102, 103, 104, 102, 101, 105]   # has duplicates
 source_B = [103, 104, 106, 107, 103, 108]          # has duplicates
 
+
+# Step 1: Remove duplicates from each source
+
+clean_A = set(source_A)
+clean_B = set(source_B)
+
+print(clean_A)
+print(clean_B)
+
