d3060f14d016c05866dd44d0a15ceaa49c692293
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-04-06 09:06:11 +0500

    Q # 01 Completed!

--- PHASE_ONE/Day_07/medium_pr_prob.py
+++ PHASE_ONE/Day_07/medium_pr_prob.py
@@ -6,3 +6,25 @@
 # union, intersection, difference (both ways), symmetric difference.
 
 
+morning_students = {"Ali", "Sara", "Fatima", "Omar", "Zara"}
+evening_students = {"Sara", "Omar", "Bilal", "Hina", "Fatima"}
+
+# Union
+union = morning_students | evening_students
+
+# Intersection 
+intersection = morning_students & evening_students
+
+# Difference
+diff_mor_to_eve = morning_students - evening_students
+diff_eve_to_mor = evening_students - morning_students 
+
+# Symmetric Difference
+symmetric = morning_students ^ evening_students
+
+print(f"Labelled Result")
+print(f"Union                : {union}")
+print(f"Intersection         : {intersection}")
+print(f"Difference Mor To Eve: {diff_mor_to_eve}")
+print(f"Difference Eve To Mor: {diff_eve_to_mor}")
+print(f"Symmetric Difference : {symmetric}")
