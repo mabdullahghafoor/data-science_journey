74ed1632fc40b96e3706e3058e06dff67e684049
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-04-06 08:59:18 +0500

    Q # 01 added

--- PHASE_ONE/Day_07/medium_pr_prob.py
+++ PHASE_ONE/Day_07/medium_pr_prob.py
@@ -1,2 +1,8 @@
 # In this program we will solve some medium level practice problems of Sets
 
+
+# Q4. Given two sets:
+# Print clearly labeled results for: 
+# union, intersection, difference (both ways), symmetric difference.
+
+
