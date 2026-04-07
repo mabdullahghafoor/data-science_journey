681abc6744d95e842bc27f30adae9b21bc4b92a1
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-04-07 22:57:35 +0500

    Loop to get keys

--- PHASE_ONE/Day_08/dictionary_method.py
+++ PHASE_ONE/Day_08/dictionary_method.py
@@ -12,3 +12,9 @@ profile = {
 print(profile.keys())     # dict_keys(['name','age','city','cgpa','skills'])
 print(profile.values())   # dict_values(['Fatima Khan', 23, 'Karachi',...])
 print(profile.items())    # dict_items([('name','Fatima Khan'),...])
+
+# ── Looping through dictionary ───────────────────────────────────
+
+# Method 1 — keys only (default)
+for key in profile:
+    print(key)
