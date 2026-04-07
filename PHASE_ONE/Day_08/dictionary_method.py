2432b885276e867755ca976627da75cb9a87388c
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-04-07 17:25:42 +0500

    Data Added

--- PHASE_ONE/Day_08/dictionary_method.py
+++ PHASE_ONE/Day_08/dictionary_method.py
@@ -1,2 +1,9 @@
 # In this program we will see some dictionaries methods
 
+profile = {
+    "name"  : "Fatima Khan",
+    "age"   : 23,
+    "city"  : "Karachi",
+    "cgpa"  : 3.95,
+    "skills": ["Python", "SQL", "ML"]
+}
