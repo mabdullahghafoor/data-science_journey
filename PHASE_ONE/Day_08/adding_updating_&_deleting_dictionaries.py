0b0fa9b6972bf5b602c26588111bb26848c86dc9
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-04-07 17:16:48 +0500

    Adding a  key-value pair

--- PHASE_ONE/Day_08/adding_updating_&_deleting_dictionaries.py
+++ PHASE_ONE/Day_08/adding_updating_&_deleting_dictionaries.py
@@ -2,3 +2,8 @@
 
 student = {"name": "Ali", "cgpa": 3.5}
 
+# ── Add new key-value pair ───────────────────────────────────────
+student["university"] = "FAST"
+student["age"]        = 22
+print(student)
+# {'name':'Ali', 'cgpa':3.5, 'university':'FAST', 'age':22}
