e0fa2906eec5bde6100628698360249c7f8ceacc
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-04-07 17:17:30 +0500

    Updating  aCGPA in a dictionary

--- PHASE_ONE/Day_08/adding_updating_&_deleting_dictionaries.py
+++ PHASE_ONE/Day_08/adding_updating_&_deleting_dictionaries.py
@@ -7,3 +7,9 @@ student["university"] = "FAST"
 student["age"]        = 22
 print(student)
 # {'name':'Ali', 'cgpa':3.5, 'university':'FAST', 'age':22}
+
+
+# ── Update existing value ────────────────────────────────────────
+student["cgpa"] = 3.87          # simply reassign
+print(student["cgpa"])          # 3.87
+
