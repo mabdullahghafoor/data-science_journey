6227cf0faa90dc12840047e5a3cb69a74ce00c76
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-04-07 17:10:17 +0500

    Data Added

--- PHASE_ONE/Day_08/creating_&_accessing_dictionaries.py
+++ PHASE_ONE/Day_08/creating_&_accessing_dictionaries.py
@@ -1,2 +1,11 @@
 # In this program we will see how to create and access dictionaries
 
+# ── Creating a Dictionary ────────────────────────────────────────
+student = {
+    "name"      : "Ali Hassan",
+    "age"       : 22,
+    "university": "FAST-NUCES",
+    "cgpa"      : 3.87,
+    "enrolled"  : True,
+    "subjects"  : ["Math", "CS", "Physics"]   # list as a value!
+}
