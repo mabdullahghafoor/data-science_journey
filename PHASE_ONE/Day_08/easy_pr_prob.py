5b15ee50799ac2196699a9c38edb1b140267facd
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-04-08 11:53:51 +0500

    Q # 01 Done

--- PHASE_ONE/Day_08/easy_pr_prob.py
+++ PHASE_ONE/Day_08/easy_pr_prob.py
@@ -3,3 +3,13 @@
 # Q1. Create a dictionary for yourself with these keys: 
 # name, age, city, favourite_language, is_student. Print every key-value pair using a loop.
 
+bio = {
+    "name" : "Abdullah",
+    "age"  : 21,
+    "city" : "Karachi",
+    "favourite_language" : "Pyhton",
+    "is_student" : True
+}
+
+for att, val in bio.items():
+    print(f"{att:>15} : {val:>15}")
