1eda6d9bf59c46112f44096c99fe621c7de22a3e
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-04-08 11:50:22 +0500

    Q # 01 Added

--- PHASE_ONE/Day_08/easy_pr_prob.py
+++ PHASE_ONE/Day_08/easy_pr_prob.py
@@ -1,2 +1,5 @@
 # In This program we will solve some easy level practice problems
 
+# Q1. Create a dictionary for yourself with these keys: 
+# name, age, city, favourite_language, is_student. Print every key-value pair using a loop.
+
