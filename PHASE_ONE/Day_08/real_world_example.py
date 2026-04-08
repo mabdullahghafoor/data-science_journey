872f8ef78df06b2f049df91bca782fe8ceee82f0
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-04-08 11:37:12 +0500

    Finding frequency of words

--- PHASE_ONE/Day_08/real_world_example.py
+++ PHASE_ONE/Day_08/real_world_example.py
@@ -2,3 +2,14 @@
 
 # ── This exact pattern is used in NLP & Text Analysis ────────────
 text = "python is great and python is easy and python is fun"
+
+words     = text.split()    # split sentence into list of words
+frequency = {}              # empty dictionary to store counts
+
+for word in words:
+    if word in frequency:
+        frequency[word] += 1    # word seen before → increment
+    else:
+        frequency[word] = 1     # first time seeing word → set to 1
+
+print(frequency)
