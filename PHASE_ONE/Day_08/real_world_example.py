65faafe0582a2acdd1240b6febbcf52136566d7a
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-04-08 11:34:59 +0500

    Data Added

--- PHASE_ONE/Day_08/real_world_example.py
+++ PHASE_ONE/Day_08/real_world_example.py
@@ -1,2 +1,4 @@
 # In this program we will see a real world problem eample in python
 
+# ── This exact pattern is used in NLP & Text Analysis ────────────
+text = "python is great and python is easy and python is fun"
