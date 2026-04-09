2ee25e347e398e90eaf0037183e191ff2d53f021
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-04-09 06:38:56 +0500

    Question Added

--- PHASE_ONE/Day_08/medium_pr_prob.py
+++ PHASE_ONE/Day_08/medium_pr_prob.py
@@ -1,1 +1,7 @@
 # In this program we will solve some medium level practice problems of Sets
+
+# Q4. Build a student gradebook using a dictionary. 
+# Store 5 students with their marks list. 
+# Loop through and print each student's 
+# average, grade, and pass/fail status.
+
