928eaef00cc9ea905170ab98d2e4b6866de89daf
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-04-09 07:27:05 +0500

    Question Added

--- PHASE_ONE/Day_08/hard_pr_prob.py
+++ PHASE_ONE/Day_08/hard_pr_prob.py
@@ -1,1 +1,11 @@
 # In this program we will solve hard level practice problems of Sets
+
+# Q7. Build a Contact Book system:
+
+#Add a contact (name, phone, email)
+#Search by name — show full details
+#Update phone or email of existing contact
+#Delete a contact
+#Display all contacts in a formatted table
+#Show total contact count
+#Handle case when contact not found gracefully
