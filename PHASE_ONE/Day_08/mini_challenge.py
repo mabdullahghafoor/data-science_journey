1911aa9afb109b588cd026707a927c9ac1a54ba0
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-04-10 23:36:57 +0500

    Question Added

--- PHASE_ONE/Day_08/mini_challenge.py
+++ PHASE_ONE/Day_08/mini_challenge.py
@@ -1,2 +1,17 @@
 # In this program we will solve a mini challenge regarding dictionaries
 
+# Student Management System — Dictionary Edition
+
+# Build a complete report that:
+
+#Calculates each student's total, percentage, grade, pass/fail
+#Finds the topper (highest percentage)
+#Finds the weakest student (lowest percentage)
+#Finds class average percentage
+#Finds the hardest subject (lowest avg across all students)
+#Finds the easiest subject (highest avg across all students)
+#Prints a fully formatted report card for EACH student
+#Prints a class summary at the bottom
+
+
+
