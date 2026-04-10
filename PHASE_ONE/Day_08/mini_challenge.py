624db01c3e46f0e9828b2628ea037862a5f4d273
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-04-10 23:37:26 +0500

    Students Data Added

--- PHASE_ONE/Day_08/mini_challenge.py
+++ PHASE_ONE/Day_08/mini_challenge.py
@@ -13,5 +13,21 @@
 #Prints a fully formatted report card for EACH student
 #Prints a class summary at the bottom
 
-
+students = {
+    "STU001": {"name": "Ali Hassan",
+               "marks": {"Math":88,"English":76,"Science":92,
+                         "Urdu":65,"CS":95}},
+    "STU002": {"name": "Sara Ahmed",
+               "marks": {"Math":65,"English":82,"Science":71,
+                         "Urdu":79,"CS":68}},
+    "STU003": {"name": "Fatima Khan",
+               "marks": {"Math":95,"English":98,"Science":100,
+                         "Urdu":88,"CS":97}},
+    "STU004": {"name": "Omar Farooq",
+               "marks": {"Math":40,"English":35,"Science":28,
+                         "Urdu":55,"CS":42}},
+    "STU005": {"name": "Zara Malik",
+               "marks": {"Math":78,"English":82,"Science":75,
+                         "Urdu":90,"CS":85}}
+}
 
