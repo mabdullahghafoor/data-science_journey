f6fa8db26fe9cb86ddafc30ef8f4a92dc47b1b60
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-04-16 21:59:40 +0500

    Practicing parameters and arguments

--- PHASE_ONE/Day_09/parameters_&_arguments.py
+++ PHASE_ONE/Day_09/parameters_&_arguments.py
@@ -1,2 +1,12 @@
 # In This Program we will see how to use parameters and arguments
 
+
+# ── Parameters = variables in function definition ────────────────
+# ── Arguments  = actual values you pass when calling ─────────────
+
+
+def greet_students (name, course):
+    print(f"Welcome {name}, You enrolled in {course}")
+
+greet_students("Ali", "Python")
+greet_students("Sara", "DSA")
