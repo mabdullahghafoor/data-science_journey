60868cf1b0cb3a174f34838809ae4c51c1534acc
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-04-17 15:44:18 +0500

    Understanding the calculate bmi function in a docstring

--- PHASE_ONE/Day_09/docstring_&_clean_code.py
+++ PHASE_ONE/Day_09/docstring_&_clean_code.py
@@ -2,3 +2,29 @@
 
 # ── Docstrings: explain your function professionally ─────────────
 # This is what appears when someone calls help(your_function)
+
+def calculate_bmi(weight_kg, height_m):
+
+    """
+    Calculate Body Mass Index (BMI).
+
+    Parameters:
+        weight_kg (float): Weight in kilograms
+        height_m  (float): Height in meters
+
+    Returns:
+        tuple: (bmi_value, category)
+
+    Example:
+        >>> bmi, category = calculate_bmi(70, 1.75)
+        >>> print(bmi)
+        22.86
+    """
+    bmi = weight_kg / (height_m ** 2)
+
+    if   bmi < 18.5: category = "Underweight"
+    elif bmi < 25.0: category = "Normal ✅"
+    elif bmi < 30.0: category = "Overweight"
+    else:            category = "Obese"
+
+    return round(bmi, 2), category
