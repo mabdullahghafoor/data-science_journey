e84180d5140984a0e2ed996b598cdc85895f5685
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-04-17 15:48:43 +0500

    Q # 01 added

--- PHASE_ONE/Day_09/easy_pr_prob.py
+++ PHASE_ONE/Day_09/easy_pr_prob.py
@@ -1,1 +1,7 @@
 # In This Program we will see how to solve easy level practice problems of functions
+
+
+# Q1. Write a function is_even(number) 
+# that returns True if the number is even and False if odd. 
+# Test with 5 different numbers.
+
