3df57d8172cbc6bb7886bbc45ceb4d965cfeab3d
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-04-17 15:41:48 +0500

    Explaining Docstring

--- PHASE_ONE/Day_09/docstring_&_clean_code.py
+++ PHASE_ONE/Day_09/docstring_&_clean_code.py
@@ -1,3 +1,4 @@
 # In This Program we will see how can we write a docstring and a clean code
 
-
+# ── Docstrings: explain your function professionally ─────────────
+# This is what appears when someone calls help(your_function)
