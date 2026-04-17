05e4ad8568cb142f685a7e935450fc94073e3a15
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-04-17 15:59:22 +0500

    Q # 01 added

--- PHASE_ONE/Day_09/medium_pr_prob.py
+++ PHASE_ONE/Day_09/medium_pr_prob.py
@@ -1,1 +1,7 @@
 # In This Program we will see how to solve medium level practice problems of functions
+
+
+# Q4. Write a function get_grade(marks) 
+# that takes marks (0-100) and returns the grade as a string. 
+# Then write another function print_report(name, marks) 
+# that calls get_grade() and prints a formatted result.
