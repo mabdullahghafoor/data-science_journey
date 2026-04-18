8c277fc75c85554148c18928dd28179ec56bafdb
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-04-18 13:07:16 +0500

    Adding a run calculator function

--- PHASE_ONE/Day_09/hard_pr_prob.py
+++ PHASE_ONE/Day_09/hard_pr_prob.py
@@ -20,10 +20,9 @@ def multiply(a,b):
 
 def divide(a,b):
     if b == 0:
-        print(f"Can't divide a number by zero(0)")
+        return "Error: Cannot divide by zero"
         
-    else:
-        return a/b
+    return a/b
     
 def calculate(a,operator,b):
 
@@ -41,4 +40,84 @@ def calculate(a,operator,b):
     
     else:
         print(f"Invalid Operator")
+
+
+def run_calculator():
+    history = []
+    while True:
+        print()
+        print(f"Choose An Option")
+        print("1 for Addition")
+        print("2 for Subtraction")
+        print("3 for Multiplication")
+        print("4 for Division")
+        print("5 for History")
+        print("6 for Exit")
+        print()
+
+        choice = int (input("Enter Your Choice (1-5): "))
+
+        if (choice == 1):
+            print()
+            num1 = int(input("Enter a first Number: "))
+            num2 = int(input("Enter a second number: "))
+            op = "+"
+
+            result = calculate(num1, op, num2)
+            print()
+            print(f"The Result Of Addition : {result}")
+
+            history.append(f"{num1} {op} {num2} = {result}")
+
+        elif (choice == 2):
+            print()
+            num1 = int(input("Enter a first Number: "))
+            num2 = int(input("Enter a second number: "))
+            op = "-"
+
+            result = calculate(num1, op, num2)
+            print()
+            print(f"The Result Of Subtraction : {result}")
+
+            history.append(f"{num1} {op} {num2} = {result}")
+
+        elif (choice == 3):
+            print()
+            num1 = int(input("Enter a first Number: "))
+            num2 = int(input("Enter a second number: "))
+            op = "*"
+
+            result = calculate(num1, op, num2)
+            print()
+            print(f"The Result Of Multiplication : {result}")
+
+            history.append(f"{num1} {op} {num2} = {result}")
+
+        elif  (choice == 4):
+            print()
+            num1 = int(input("Enter a first Number: "))
+            num2 = int(input("Enter a second number: "))
+            op = "/"
+
+            result = calculate(num1, op, num2)
+            print()
+            print(f"The Result Of Division : {result}")
+
+            history.append(f"{num1} {op} {num2} = {result}")
+
+        elif choice == 5:
+            print()
+            print("History")
+            print()
+            for items in history:
+                
+                print(items)
+
+        elif choice == 6:
+            break
+
+        else:
+            print()
+            print("Invalid Choice ")
+
 
