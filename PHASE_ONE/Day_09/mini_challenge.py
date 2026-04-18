46f5bd7e4c7bd32fe47d973e627633fc7b287ed6
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-04-18 13:47:51 +0500

    Again re-updated get status function

--- PHASE_ONE/Day_09/mini_challenge.py
+++ PHASE_ONE/Day_09/mini_challenge.py
@@ -24,7 +24,7 @@ def calculate_percentage(marks, total_per_subject=100):
     """Calculate percentage from marks list."""
 
     obtained_marks = sum(marks)
-    total_marks = len(marks) * 100
+    total_marks = len(marks) * total_per_subject
     percentage = (obtained_marks/total_marks)*100
 
     return percentage
@@ -41,9 +41,10 @@ def get_grade(percentage):
 
 
 def get_status(marks, passing_mark=40, passing_percent=50):
-    """Return Pass/Fail based on all subjects and percentage."""
+    
+    percentage = calculate_percentage(marks)
 
-    if (marks >= 40) and (calculate_percentage(marks) >= 50):
+    if percentage >= passing_percent and all(mark >= passing_mark for mark in marks):
         return "Status: PASS✅"
     else:
         return "Status: FAIL❌"
