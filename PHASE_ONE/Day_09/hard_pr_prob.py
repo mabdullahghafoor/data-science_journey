0393fff05c8664ae15d6dfca1c7b17c53fc2d1e7
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-04-18 11:33:31 +0500

    Question Added

--- PHASE_ONE/Day_09/hard_pr_prob.py
+++ PHASE_ONE/Day_09/hard_pr_prob.py
@@ -1,1 +1,9 @@
 # In This Program we will see how to solve hard level practice problems of functions
+
+# Q7. Build a calculator using functions:
+
+#add(a, b), subtract(a, b), multiply(a, b), divide(a, b)
+#calculate(a, operator, b) — calls the right function above
+#run_calculator() — menu loop, keeps running until user exits
+#Handle division by zero gracefully
+#Display history of all calculations in current session
