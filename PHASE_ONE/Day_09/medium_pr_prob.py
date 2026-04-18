e14e8eb33e57b29820d2c34264cd934b1db9affa
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-04-18 10:30:08 +0500

    Making a get_grade function

--- PHASE_ONE/Day_09/medium_pr_prob.py
+++ PHASE_ONE/Day_09/medium_pr_prob.py
@@ -5,3 +5,20 @@
 # that takes marks (0-100) and returns the grade as a string. 
 # Then write another function print_report(name, marks) 
 # that calls get_grade() and prints a formatted result.
+
+
+def get_grade(marks):
+
+    if marks <= 90:
+        return "Grade A+"
+    elif marks <= 80:
+        return "Grade A"
+    elif marks <= 70:
+        return "Grade B"
+    elif marks <= 60:
+        return "Grade C"
+    elif marks <= 50:
+        return "Grade D"
+    else:
+        return "Garde F"
+    
