e760a59aa85cdd1a0ccd9f1437fb97785ef568b7
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-04-19 22:56:01 +0500

    Question # 01 Added

--- PHASE_ONE/Day_10/easy_pr_prob.py
+++ PHASE_ONE/Day_10/easy_pr_prob.py
@@ -1,1 +1,9 @@
 # In This Program we will see how to solve easy level practice problems of functions
+
+#Q1. Write lambda functions for:
+
+#Cube of a number
+#Check if a number is positive
+#Convert Celsius to Fahrenheit
+#Find the longer of two strings
+
