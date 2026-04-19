a39a25cb0b0f79946b8f85abab0739486b7a6180
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-04-19 21:56:22 +0500

    Update lamda_with_map_filter_sort.py

--- PHASE_ONE/Day_10/lamda_with_map_filter_sort.py
+++ PHASE_ONE/Day_10/lamda_with_map_filter_sort.py
@@ -1,2 +1,4 @@
 # In this we will se how to use basic lambda with map plus filter plus sort function together
 
+# ── Real Data Science pipeline in 3 lines ────────────────────────
+# This is EXACTLY how you process data in pandas/python
