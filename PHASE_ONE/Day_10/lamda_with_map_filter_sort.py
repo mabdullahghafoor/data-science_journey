c89d18a3cc053770307bb64a2cdb27175ea6a219
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-04-19 21:56:40 +0500

    Update lamda_with_map_filter_sort.py

--- PHASE_ONE/Day_10/lamda_with_map_filter_sort.py
+++ PHASE_ONE/Day_10/lamda_with_map_filter_sort.py
@@ -2,3 +2,5 @@
 
 # ── Real Data Science pipeline in 3 lines ────────────────────────
 # This is EXACTLY how you process data in pandas/python
+
+raw_marks = [88, 45, 92, 32, 78, 65, 55, 90, 28, 73, 38, 82]
