28cb5e19e920b4aaca8e944f6727483426aeceb9
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-04-19 23:32:12 +0500

    Description Added

--- PHASE_ONE/Day_10/medium_pr_prob.py
+++ PHASE_ONE/Day_10/medium_pr_prob.py
@@ -1,1 +1,8 @@
 # In This Program we will see how to solve medium level practice problems of lamda functions
+
+#Q4. Given this list of students:
+#Use lambda with sorted(), filter(), map() to:
+
+#Sort by marks (highest first)
+#Filter only students from Karachi
+#Add 5 bonus marks to everyone
