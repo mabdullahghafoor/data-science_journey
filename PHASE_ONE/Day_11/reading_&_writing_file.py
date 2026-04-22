dffeb7cda2ea26b7b225bb6e7c5a25bac638b14a
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-04-23 00:03:14 +0500

    File Write Command Sucesssfully

--- PHASE_ONE/Day_11/reading_&_writing_file.py
+++ PHASE_ONE/Day_11/reading_&_writing_file.py
@@ -2,3 +2,12 @@
 
 # ── Writing to a file ────────────────────────────────────────────
 # "w" mode → creates file if not exists, OVERWRITES if exists
+
+with open("Students.txt", "w") as file:
+
+    file.write("Ali Hasan\n")
+    file.write("Sara Ahmad\n")
+    file.write("Fatima Khan\n")
+    file.write("Omar Farroq\n")
+
+print("File Written Successfully")
