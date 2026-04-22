b3dd9e5e8066370f66f782409cab1648c73cc8e9
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-04-22 12:52:53 +0500

    Question Added

--- PHASE_ONE/Day_10/hard_pr_prob.py
+++ PHASE_ONE/Day_10/hard_pr_prob.py
@@ -1,2 +1,9 @@
 # In This Program we will see how to solve hard level practice problems of lamda functions
 
+#Q7. You have this sales data:
+#Using only lambda, map, filter, sorted:
+
+#Calculate total revenue per product (price × qty)
+#Filter products with revenue > PKR 100,000#
+#Sort by revenue highest first
+#Print a formatted sales report
