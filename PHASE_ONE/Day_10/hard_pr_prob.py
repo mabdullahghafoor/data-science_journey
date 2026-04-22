cc1943ab6ddd619b5f528d2177cc4fdd240816d0
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-04-22 12:53:31 +0500

    Data added

--- PHASE_ONE/Day_10/hard_pr_prob.py
+++ PHASE_ONE/Day_10/hard_pr_prob.py
@@ -7,3 +7,13 @@
 #Filter products with revenue > PKR 100,000#
 #Sort by revenue highest first
 #Print a formatted sales report
+
+
+sales = [
+    {"product": "Laptop",   "price": 85000, "qty": 5,  "city": "Karachi"},
+    {"product": "Mouse",    "price":  1500, "qty": 50, "city": "Lahore"},
+    {"product": "Keyboard", "price":  3500, "qty": 30, "city": "Karachi"},
+    {"product": "Monitor",  "price": 45000, "qty": 8,  "city": "Lahore"},
+    {"product": "Webcam",   "price":  8000, "qty": 15, "city": "Islamabad"},
+]
+
