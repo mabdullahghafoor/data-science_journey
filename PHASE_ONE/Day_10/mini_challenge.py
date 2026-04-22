2b8125907dc197cb338a4adbe78875f4ba7f93f2
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-04-22 13:53:53 +0500

    Question Added

--- PHASE_ONE/Day_10/mini_challenge.py
+++ PHASE_ONE/Day_10/mini_challenge.py
@@ -1,1 +1,11 @@
 # In This Program we will see how to solve a mini challenge related to lamda functions
+
+#"Data Pipeline — Lambda Style"
+
+#Using lambdas with map/filter/sorted, build a pipeline that:
+
+#Cleans names (strip spaces + proper Title Case)
+#Adds percentage and grade to each student record
+#Filters only fee-paid students
+#Sorts by marks highest first
+#Prints a final clean formatted report
