92b9f6e5d3f779f4cc8c6c4a4731ed410ccd3678
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-04-23 10:13:56 +0500

    "w" mode — DANGEROUS if file exists

--- PHASE_ONE/Day_11/append_vs_write.py
+++ PHASE_ONE/Day_11/append_vs_write.py
@@ -1,3 +1,12 @@
 #In this program we will see how to append and write a file
 
 # ── "w" mode — DANGEROUS if file exists ──────────────────────────
+
+with open("log.txt", "w") as file:
+
+    file.write("Entry 1\n")
+
+
+with open("log.txt", "w") as file:
+    file.write("Entry 2\n") # ❌ First entry is GONE!
+
