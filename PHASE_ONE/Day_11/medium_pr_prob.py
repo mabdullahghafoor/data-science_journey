57cdf383653694d9d6f9a79f07a59d3b92ab42f1
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-04-23 19:36:30 +0500

    Q # 1 aded

--- PHASE_ONE/Day_11/medium_pr_prob.py
+++ PHASE_ONE/Day_11/medium_pr_prob.py
@@ -1,1 +1,9 @@
 # In This Program we will see how to solve medium level practice problems of File Handling
+
+#Q4. Build a To-Do List that saves to a file:
+
+#add_task(task) → appends to todos.txt
+#show_tasks() → reads and prints all tasks with numbers
+#clear_tasks() → empties the file
+#Tasks persist between program runs!
+
