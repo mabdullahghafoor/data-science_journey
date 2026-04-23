746de905749c35c7d27b37100fa218851e505548
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-04-23 12:42:28 +0500

    Q # 01 added

--- PHASE_ONE/Day_11/easy_pr_prob.py
+++ PHASE_ONE/Day_11/easy_pr_prob.py
@@ -1,2 +1,8 @@
 # In This Program we will see how to solve easy level practice problems of File Handling
 
+#Q1. Write a program that:
+
+#Creates a file called my_notes.txt
+#Writes 5 lines of text to it
+#Reads it back and prints each line
+
