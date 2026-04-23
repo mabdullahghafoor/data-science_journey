407b1bc6bb7959e156c94671887ba2302bfaa9eb
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-04-23 12:53:55 +0500

    Q # 01 done and dusted

--- PHASE_ONE/Day_11/easy_pr_prob.py
+++ PHASE_ONE/Day_11/easy_pr_prob.py
@@ -9,3 +9,18 @@
 import os
 
 filename = "my_notes.txt"
+
+
+with open(filename, "a") as file:
+    file.write("Line # 01\n")
+    file.write("Line # 02\n")
+    file.write("Line # 03\n")
+    file.write("Line # 04\n")
+    file.write("Line # 05\n")
+print("File Written")
+print()
+with open(filename, "r") as file:
+
+    for line in file:
+        print(line.strip())
+
