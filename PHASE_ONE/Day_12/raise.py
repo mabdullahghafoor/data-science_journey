c67592cfa9b98b4e1fed36d8f88ef762ffef4c6b
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-04-24 23:49:55 +0500

    Update raise.py

--- PHASE_ONE/Day_12/raise.py
+++ PHASE_ONE/Day_12/raise.py
@@ -1,2 +1,4 @@
 # In this we will se  how to use raise# In this we will se  how to use raise
+# In this we will se  how to use raise
 
+# ── You can CREATE and RAISE your own exceptions ──────────────────
