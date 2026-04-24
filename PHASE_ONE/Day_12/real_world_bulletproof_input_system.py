b53cee40139f5fec7366a8907016884e2d029652
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-04-24 23:58:38 +0500

    Update real_world_bulletproof_input_system.py

--- PHASE_ONE/Day_12/real_world_bulletproof_input_system.py
+++ PHASE_ONE/Day_12/real_world_bulletproof_input_system.py
@@ -29,3 +29,5 @@ def get_float_input(prompt):
     while True:
         try:
             return float(input(prompt))
+        except ValueError:
+            print("  ❌ Please enter a valid decimal number.")
