a9939d24efef2dc2f294f9a038888bc8b74821fc
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-04-25 20:24:37 +0500

    Update medium_pr_prob.py

--- PHASE_ONE/Day_12/medium_pr_prob.py
+++ PHASE_ONE/Day_12/medium_pr_prob.py
@@ -1,2 +1,8 @@
 # In This Program we will see how to solve easy level practice problems of Exception Handling Handling
 
+#Q4. Build a safe_list_access(lst, index) function that:
+
+#Returns the item at given index
+#Handles IndexError if index is out of range
+#Handles TypeError if index is not an integer
+#Returns None on any error with a helpful message
