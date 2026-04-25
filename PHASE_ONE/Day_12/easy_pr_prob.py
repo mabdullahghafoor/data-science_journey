65d4a3a62cf22ab434adc7f518bc0479b2bf4b6b
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-04-25 19:47:14 +0500

    Update easy_pr_prob.py

--- PHASE_ONE/Day_12/easy_pr_prob.py
+++ PHASE_ONE/Day_12/easy_pr_prob.py
@@ -1,3 +1,7 @@
 # In This Program we will see how to solve easy level practice problems of Exception Handling Handling
 
 
+#Q1. Write a program that asks user for two numbers 
+# and divides them.
+# Handle ValueError (non-numeric input) and 
+# ZeroDivisionError (division by zero) separately.
