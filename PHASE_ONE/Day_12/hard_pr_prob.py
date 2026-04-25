8cfc2f5a703b1ce906b9a2fb218d00d9ac44b829
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-04-25 20:52:47 +0500

    Update hard_pr_prob.py

--- PHASE_ONE/Day_12/hard_pr_prob.py
+++ PHASE_ONE/Day_12/hard_pr_prob.py
@@ -1,1 +1,11 @@
 # In This Program we will see how to solve medium level practice problems of Exception Handling Handling
+
+#Q7. Build a robust calculator that:
+
+#Never crashes on any input
+#Handles: division by zero, invalid numbers,
+#invalid operators, empty input
+#Logs every error to error_log.txt with timestamp
+#Logs every successful calculation to calc_history.txt
+#Keeps running until user types "exit"
+
