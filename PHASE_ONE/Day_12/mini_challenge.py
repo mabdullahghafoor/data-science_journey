cc5032bb8be43de88b534842bdc3711e09a66ed0
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-04-25 21:00:16 +0500

    Update mini_challenge.py

--- PHASE_ONE/Day_12/mini_challenge.py
+++ PHASE_ONE/Day_12/mini_challenge.py
@@ -1,2 +1,19 @@
 # In this we will solve a mini challenge
 
+#"Bulletproof Student Registration System"
+
+#Build a complete student registration system where
+#nothing can crash it:
+
+#Test your system with deliberately bad inputs:
+
+#Letters where numbers expected
+#CGPA of 9.5
+#Phone number with letters
+#Empty name
+#Invalid email
+
+#Every case should show a friendly error message
+#and ask again — never crash!
+
+
