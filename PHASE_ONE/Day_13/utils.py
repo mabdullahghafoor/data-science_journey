089a295565cf5b0b4922f9b90e67a64d0e410ad6
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-05-06 13:20:53 +0500

    Is_Passing Function

--- PHASE_ONE/Day_13/utils.py
+++ PHASE_ONE/Day_13/utils.py
@@ -25,3 +25,10 @@ def get_grade(percentage):
     elif percentage >= 50: return "D"
     else:                  return "F"
 
+def is_passing(marks, pass_mark=40, pass_percent=50):
+    """Check if student has passed all subjects."""
+    all_passed = all(m >= pass_mark for m in marks)
+    percentage = calculate_percentage(marks)
+    return all_passed and percentage >= pass_percent
+
+PI = 3.14159     # module-level constant
