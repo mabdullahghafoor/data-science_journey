544334b730c6546e3a45e05faf50eeff1a2e70bb
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-05-08 22:59:02 +0500

    Update main.py

--- PHASE_ONE/Day_13/students_system/main.py
+++ PHASE_ONE/Day_13/students_system/main.py
@@ -8,3 +8,4 @@ from students_system.reports import generate_report_header
 generate_report_header("Student Performance Report")
 marks = [88, 76, 92, 65, 95]
 pct   = utils.calculate_percentage(marks)
+grade = assign_grade(pct)
