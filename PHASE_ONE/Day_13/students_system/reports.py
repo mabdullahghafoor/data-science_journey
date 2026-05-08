dcab2f2ad026ac5e7e5bb7422bd6c88eb4f466e3
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-05-08 22:57:30 +0500

    Update reports.py

--- PHASE_ONE/Day_13/students_system/reports.py
+++ PHASE_ONE/Day_13/students_system/reports.py
@@ -5,3 +5,7 @@ from datetime import datetime
 
 def generate_report_header(title):
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M")
+    print(f"\n{'═'*45}")
+    print(f"  {title}")
+    print(f"  Generated: {timestamp}")
+    print(f"{'═'*45}")
