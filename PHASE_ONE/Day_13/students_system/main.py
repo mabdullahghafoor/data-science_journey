ec4c11afb544d3ce9448b97c4c63e495e659e8d3
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-05-08 22:58:58 +0500

    Update main.py

--- PHASE_ONE/Day_13/students_system/main.py
+++ PHASE_ONE/Day_13/students_system/main.py
@@ -6,3 +6,4 @@ from students_system.grades import assign_grade
 from students_system.reports import generate_report_header
 
 generate_report_header("Student Performance Report")
+marks = [88, 76, 92, 65, 95]
