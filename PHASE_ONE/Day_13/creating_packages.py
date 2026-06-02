a1751719b1225670fd2caac5c1b50b0de115c836
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-06-02 15:41:33 +0500

    Update creating_packages.py

--- PHASE_ONE/Day_13/creating_packages.py
+++ PHASE_ONE/Day_13/creating_packages.py
@@ -10,3 +10,11 @@
 #
 
 # FILE: grades.py
+def assign_grade(percentage):
+    grades = {
+        range(90, 101): "A+",
+        range(80, 90) : "A",
+        range(70, 80) : "B",
+        range(60, 70) : "C",
+        range(50, 60) : "D",
+    }
