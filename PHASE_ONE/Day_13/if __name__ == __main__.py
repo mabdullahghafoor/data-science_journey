5f0918e8d63d738be03a0e70776d11df880b4b57
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-06-02 16:03:31 +0500

    Update if __name__ == __main__.py

--- PHASE_ONE/Day_13/if __name__ == __main__.py
+++ PHASE_ONE/Day_13/if __name__ == __main__.py
@@ -14,3 +14,4 @@ def multiply(a, b):
 # ── This block ONLY runs when you run THIS file directly ──────────
 # It does NOT run when this file is imported as a module
 if __name__ == "__main__":
+    # Test code / demo code here
