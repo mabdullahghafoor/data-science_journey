61a37fd9e743baaee4c9c98b8c310a9de1472c81
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-06-10 23:46:00 +0500

    Update generator.py

--- PHASE_ONE/password-generator/generator.py
+++ PHASE_ONE/password-generator/generator.py
@@ -6,3 +6,6 @@ def generate_password(length=12,
                       use_upper=True,
                       use_digits=True,
                       use_symbols=True):
+    """
+    Generate a secure random password.
+    """
