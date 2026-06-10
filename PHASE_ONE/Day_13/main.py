6e71a575dd1f130612d91b9c1e3b5a0addd24a60
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-06-10 23:14:46 +0500

    Update main.py

--- PHASE_ONE/Day_13/main.py
+++ PHASE_ONE/Day_13/main.py
@@ -2,3 +2,4 @@ import string_utils
 
 print(string_utils.is_palindrome("madam"))
 print(string_utils.word_count("apple banana apple mango banana"))
+print(string_utils.capitalize_words("hello world from python"))
