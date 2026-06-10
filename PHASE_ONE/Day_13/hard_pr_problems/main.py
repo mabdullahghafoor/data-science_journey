f065214d77a86dbe3d6d408430f3774e61462b64
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-06-10 23:25:54 +0500

    Update main.py

--- PHASE_ONE/Day_13/hard_pr_problems/main.py
+++ PHASE_ONE/Day_13/hard_pr_problems/main.py
@@ -2,3 +2,5 @@ from utils import calculate_percentage, get_grade
 from validators import validate_name, validate_marks
 from reporter import print_card, print_summary
 
+name = "Ali"
+marks = [85, 90, 78, 88, 92]
