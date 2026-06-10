c83dc931ad712b7b9f78c819ccd03db125bd7d19
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-06-10 23:26:27 +0500

    Update reporter.py

--- PHASE_ONE/Day_13/hard_pr_problems/reporter.py
+++ PHASE_ONE/Day_13/hard_pr_problems/reporter.py
@@ -7,3 +7,7 @@ def print_card(name, percentage, grade):
     """
     Print student's report card.
     """
+    print("\n----- Report Card -----")
+    print("Name:", name)
+    print("Percentage:", round(percentage, 2))
+    print("Grade:", grade)
