5af4175b10bf23dd27ba82d9343f62f924963db1
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-06-10 23:14:48 +0500

    Update main.py

--- PHASE_ONE/Day_13/main.py
+++ PHASE_ONE/Day_13/main.py
@@ -3,3 +3,4 @@ import string_utils
 print(string_utils.is_palindrome("madam"))
 print(string_utils.word_count("apple banana apple mango banana"))
 print(string_utils.capitalize_words("hello world from python"))
+print(string_utils.remove_duplicates("apple banana apple mango banana"))
