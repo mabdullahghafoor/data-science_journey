054ea7afe15a65e892b86e631232686a89056bf4
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-06-10 23:27:56 +0500

    Update validators.py

--- PHASE_ONE/Day_13/hard_pr_problems/validators.py
+++ PHASE_ONE/Day_13/hard_pr_problems/validators.py
@@ -14,3 +14,6 @@ def validate_marks(marks):
     """
     Check whether all marks are between 0 and 100.
     """
+    for mark in marks:
+        if mark < 0 or mark > 100:
+            return False
