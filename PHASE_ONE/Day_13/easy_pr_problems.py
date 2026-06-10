3acaff83dbc2ec4666cdd772455c77d1e77b3d55
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-06-10 15:38:46 +0500

    Update easy_pr_problems.py

--- PHASE_ONE/Day_13/easy_pr_problems.py
+++ PHASE_ONE/Day_13/easy_pr_problems.py
@@ -1,2 +1,7 @@
 # In this we will solve some easy level problems
 
+# Q1. Import the math module and write a program that:
+
+#Calculates square root of 10 numbers
+#Finds the ceiling and floor of 5 decimal numbers
+#Calculates log base 10 of: 10, 100, 1000
