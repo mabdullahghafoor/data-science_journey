610e62878ac94ad79bd4325c2f532dd2dd1fa5b6
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-06-12 19:18:22 +0500

    Update real_world_prob.py

--- PHASE_ONE/Day_14/real_world_prob.py
+++ PHASE_ONE/Day_14/real_world_prob.py
@@ -8,3 +8,8 @@ class Student:
     total_students = 0
 
     def __init__(self, student_id, name, age):
+        self.student_id  = student_id
+        self.name        = name
+        self.age         = age
+        self.__marks     = {}       # private: subject → marks
+        Student.total_students += 1
