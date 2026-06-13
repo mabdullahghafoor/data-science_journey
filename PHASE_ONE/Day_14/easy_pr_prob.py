0ffe2ace7ef0fd01949fdeb45ae1b7d1ec17d19a
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-06-13 23:41:26 +0500

    Update easy_pr_prob.py

--- PHASE_ONE/Day_14/easy_pr_prob.py
+++ PHASE_ONE/Day_14/easy_pr_prob.py
@@ -2,3 +2,7 @@
 
 #Q1. Create a Car class with:
 
+#Attributes: brand, model, year, speed
+#Method accelerate(amount) → increases speed
+#Method brake(amount) → decreases speed (not below 0)
+#Method get_info() → prints all details
