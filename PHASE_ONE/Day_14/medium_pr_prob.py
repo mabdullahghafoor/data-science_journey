ff621f58ca47790b7626d69530af053eb55eb3ac
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-06-19 22:58:02 +0500

    Update medium_pr_prob.py

--- PHASE_ONE/Day_14/medium_pr_prob.py
+++ PHASE_ONE/Day_14/medium_pr_prob.py
@@ -73,7 +73,6 @@ class Teacher(Person):
     def __init__(self, name, age):
         super().__init__(name, age)
 
-    def introduce(self,name):
         self.name = name
         print(f"Hello {self.name}, This is Teacher's class ")
 
