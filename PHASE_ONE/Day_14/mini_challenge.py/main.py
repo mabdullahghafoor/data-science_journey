93e98e82a8244bbcc2a124d4b5acf0a144732a4f
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-06-25 23:44:03 +0500

    Update main.py

--- PHASE_ONE/Day_14/mini_challenge.py/main.py
+++ PHASE_ONE/Day_14/mini_challenge.py/main.py
@@ -10,3 +10,7 @@ s1.add_marks("Math", 90)
 s1.add_marks("Python", 85)
 s1.add_marks("OOP", 80)
 
+s2 = Student(2, "Sara")
+s2.add_marks("Math", 95)
+s2.add_marks("Python", 92)
+s2.add_marks("OOP", 89)
