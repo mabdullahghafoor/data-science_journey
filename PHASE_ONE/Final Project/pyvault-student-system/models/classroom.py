cd16e97782cd094657159a3e9a41e2792f845c41
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-07-12 02:08:36 +0500

    Update classroom.py

--- PHASE_ONE/Final Project/pyvault-student-system/models/classroom.py
+++ PHASE_ONE/Final Project/pyvault-student-system/models/classroom.py
@@ -8,3 +8,16 @@ import json
 import os
 from models.students import Student
 
+
+class Classroom:
+    """
+    Manages a collection of students.
+    Demonstrates: OOP, DSA algorithms, File I/O
+    """
+
+    DATA_FILE = "data/students.json"
+
+    def __init__(self):
+        self.__students = {}    # hashmap: id → Student object
+        self.__load()           # load from file on startup
+
