a7c374103dea480b616c9203a633c6353bb52a8d
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-07-11 19:56:54 +0500

    Update sorting_algos.py

--- PHASE_ONE/Day_15/sorting_algos.py
+++ PHASE_ONE/Day_15/sorting_algos.py
@@ -16,3 +16,7 @@ def bubble_sort(arr):
     for i in range(n):
         swapped = False
 
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                swapped = True
