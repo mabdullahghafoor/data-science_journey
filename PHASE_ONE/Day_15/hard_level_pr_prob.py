aa169833d29b7e7eb9b9c4f6cbf6f7b977dc914f
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-07-11 20:02:23 +0500

    Update hard_level_pr_prob.py

--- PHASE_ONE/Day_15/hard_level_pr_prob.py
+++ PHASE_ONE/Day_15/hard_level_pr_prob.py
@@ -4,3 +4,10 @@
 #Q7. Implement merge sort from scratch.
 #Then sort a list of student dictionaries
 #by percentage using merge sort:
+
+students = [
+    {"name": "Ali",    "marks": [88, 76, 92]},
+    {"name": "Sara",   "marks": [65, 82, 71]},
+    {"name": "Fatima", "marks": [95, 98, 100]},
+    {"name": "Omar",   "marks": [40, 35, 28]},
+]
