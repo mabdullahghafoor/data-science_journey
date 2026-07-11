8841063dbba12b9abfe7cf9fce0ccfe052b89d21
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-07-11 20:03:22 +0500

    Update mini_challenge.py

--- PHASE_ONE/Day_15/mini_challenge.py
+++ PHASE_ONE/Day_15/mini_challenge.py
@@ -2,3 +2,15 @@
 
 
 #"DSA Student Analytics"
+
+#Combine ALL DSA concepts in one program:
+#python# Use these structures:
+
+#Build a program that:
+
+#Uses Array operations to find max/min without built-ins
+#Uses Hashmap for grade frequency counting
+#Uses Stack to show top 5 students (push then pop)
+#Uses Queue to simulate result announcement order
+#Uses Bubble/Merge sort to rank students
+#Prints complete analysis report
