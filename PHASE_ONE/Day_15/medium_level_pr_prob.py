ade3ee5621e2ada9e1876281a9032aa5c74519e2
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-07-11 20:00:34 +0500

    Update medium_level_pr_prob.py

--- PHASE_ONE/Day_15/medium_level_pr_prob.py
+++ PHASE_ONE/Day_15/medium_level_pr_prob.py
@@ -9,3 +9,10 @@ def bubble_sort(arr):
     swaps = 0
     n = len(arr)
 
+    for i in range(n):
+        for j in range(n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swaps += 1
+
+    return arr, swaps
