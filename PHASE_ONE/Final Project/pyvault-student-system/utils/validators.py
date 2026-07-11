3eaceaf5d70dda28a14f81271655ef97cc801956
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-07-12 02:09:26 +0500

    Update validators.py

--- PHASE_ONE/Final Project/pyvault-student-system/utils/validators.py
+++ PHASE_ONE/Final Project/pyvault-student-system/utils/validators.py
@@ -3,3 +3,4 @@ def get_float(prompt, min_val=None, max_val=None):
     while True:
             value = float(input(prompt).strip())
             if min_val is not None and value < min_val:
+            if max_val is not None and value > max_val:
