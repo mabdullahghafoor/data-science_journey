11fc3d04034cf9c632e8edd96a83523f75fd18b8
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-07-12 02:09:36 +0500

    Update validators.py

--- PHASE_ONE/Final Project/pyvault-student-system/utils/validators.py
+++ PHASE_ONE/Final Project/pyvault-student-system/utils/validators.py
@@ -5,5 +5,7 @@ def get_float(prompt, min_val=None, max_val=None):
             if min_val is not None and value < min_val:
             if max_val is not None and value > max_val:
             return value
+            print(f"  ❌ Invalid: {e}. Try again.")
+def validate_email(email):
         raise ValueError(f"Invalid email: {email}")
 
