9a4b060bf87e20536d2af18ae3326337c0140ed2
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-07-11 19:59:49 +0500

    Update easy_level_pr_prob.py

--- PHASE_ONE/Day_15/easy_level_pr_prob.py
+++ PHASE_ONE/Day_15/easy_level_pr_prob.py
@@ -39,3 +39,11 @@ def binary_search(arr, target):
     while left <= right:
         mid = (left + right) // 2
 
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return -1
