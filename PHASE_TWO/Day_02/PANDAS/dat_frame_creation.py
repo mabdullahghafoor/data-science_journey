e7d9d670897c048e764244066e60cf7688b8bc35
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-07-14 23:29:44 +0500

    Update dat_frame_creation.py

--- PHASE_TWO/Day_02/PANDAS/dat_frame_creation.py
+++ PHASE_TWO/Day_02/PANDAS/dat_frame_creation.py
@@ -18,3 +18,4 @@ print(df)
 print(df.shape)    # (4, 4) — 4 rows, 4 columns
 print(df.columns)  # Index(['name', 'age', 'city', 'salary'])
 print(df.dtypes)   # name: object, age: int64, city: object, salary: int64
+print(df.index)    # RangeIndex(start=0, stop=4, step=1)
