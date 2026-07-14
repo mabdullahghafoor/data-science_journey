8161be69389e6cf03f918f614acacd59e1b54868
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-07-14 23:29:05 +0500

    Update dat_frame_creation.py

--- PHASE_TWO/Day_02/PANDAS/dat_frame_creation.py
+++ PHASE_TWO/Day_02/PANDAS/dat_frame_creation.py
@@ -3,3 +3,4 @@ data = {
     "name":   ["Ali", "Sara", "Ahmed", "Zara"],
     "age":    [23, 27, 22, 25],
     "city":   ["Karachi", "Lahore", "Karachi", "Islamabad"],
+    "salary": [45000, 62000, 38000, 71000]
