cc9d6ab748e07332017d62f6ba86cadfa33fadaf
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-07-19 00:34:10 +0500

    Delete dat_frame_creation.py

--- PHASE_TWO/Day_02/PANDAS/dat_frame_creation.py
+++ PHASE_TWO/Day_02/PANDAS/dat_frame_creation.py
@@ -1,10 +1,3 @@
-df = pd.DataFrame(data)
-print(df)
-#     name  age       city  salary
-# 0    Ali   23    Karachi   45000
-# 1   Sara   27     Lahore   62000
-# 2  Ahmed   22    Karachi   38000
-# 3   Zara   25  Islamabad   71000
 
 # Key attributes
 print(df.shape)    # (4, 4) — 4 rows, 4 columns
