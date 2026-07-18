3c9404c667eae1dcba776bbf649ab05bdeae922b
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-07-19 00:34:53 +0500

    Delete series.py

--- PHASE_TWO/Day_02/PANDAS/series.py
+++ PHASE_TWO/Day_02/PANDAS/series.py
@@ -1,13 +1,3 @@
-print(s.index)    # RangeIndex(start=0, stop=5, step=1)
-print(s.dtype)    # int64
-
-# Custom index
-s2 = pd.Series([85, 92, 78], index=["Alice", "Bob", "Carol"])
-print(s2)
-# Alice    85
-# Bob      92
-# Carol    78
-
 print(s2["Bob"])   # 92
 print(s2[s2 > 80]) # Alice 85, Bob 92  ← boolean indexing works here too
 
