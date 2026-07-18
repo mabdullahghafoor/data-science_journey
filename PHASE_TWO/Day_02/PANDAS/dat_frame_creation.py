c6753b34512c4443e5812207d587b14ceea8afc1
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-07-19 00:33:55 +0500

    Delete dat_frame_creation.py

--- PHASE_TWO/Day_02/PANDAS/dat_frame_creation.py
+++ PHASE_TWO/Day_02/PANDAS/dat_frame_creation.py
@@ -1,6 +1,3 @@
-# From a dictionary — most common way
-data = {
-    "name":   ["Ali", "Sara", "Ahmed", "Zara"],
     "age":    [23, 27, 22, 25],
     "city":   ["Karachi", "Lahore", "Karachi", "Islamabad"],
     "salary": [45000, 62000, 38000, 71000]
