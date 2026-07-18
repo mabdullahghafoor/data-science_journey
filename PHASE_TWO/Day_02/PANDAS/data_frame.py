49b9e0670a8348c3bea7d3f8e113dcaa3becf7f0
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-07-19 01:28:32 +0500

    Update data_frame.py

--- PHASE_TWO/Day_02/PANDAS/data_frame.py
+++ PHASE_TWO/Day_02/PANDAS/data_frame.py
@@ -3,3 +3,10 @@
 import pandas as pd
 import numpy as np
 
+data = {
+    "name":   ["Ali", "Sara", "Ahmed", "Zara"],
+    "age":    [23, 27, 22, 25],
+    "city":   ["Karachi", "Lahore", "Karachi", "Islamabad"],
+    "salary": [45000, 62000, 38000, 71000]
+}
+
