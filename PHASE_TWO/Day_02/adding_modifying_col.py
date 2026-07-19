2048e8b27ae31a4778c0713d56556934b6daa6bc
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-07-19 15:41:26 +0500

    Update adding_modifying_col.py

--- PHASE_TWO/Day_02/adding_modifying_col.py
+++ PHASE_TWO/Day_02/adding_modifying_col.py
@@ -20,3 +20,6 @@ df["AgeGroup"] = pd.cut(df["Age"] , bins = [0 , 18 , 40 , 60] ,
                         labels = ["Child" , "Adult" , "Senior"])
 
 
+# Using np.where for a simple binary label
+
+df["Adult"] = np.where(df["Age"] >= 18 , "Yes" , "No")
