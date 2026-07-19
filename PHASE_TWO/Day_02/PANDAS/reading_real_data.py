c8ad3fee9980374291ccfeb302a3f9870db0793f
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-07-19 14:14:54 +0500

    Update reading_real_data.py

--- PHASE_TWO/Day_02/PANDAS/reading_real_data.py
+++ PHASE_TWO/Day_02/PANDAS/reading_real_data.py
@@ -10,3 +10,8 @@ df = pd.read_csv("titanic.csv")
 
 # Read from a URL directly (useful in Colab/Kaggle)
 
+url = "https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv"
+
+df = pd.read_csv(url)
+
+
