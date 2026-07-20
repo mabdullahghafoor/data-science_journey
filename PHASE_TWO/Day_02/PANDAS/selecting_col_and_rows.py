f0c212a4c3ff5b286af4d1772b4f7828df921189
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-07-20 22:47:15 +0500

    Update selecting_col_and_rows.py

--- PHASE_TWO/Day_02/PANDAS/selecting_col_and_rows.py
+++ PHASE_TWO/Day_02/PANDAS/selecting_col_and_rows.py
@@ -32,3 +32,14 @@ df.loc[df["Age"] > 30 , "Name"] # Rows where age >30 only name col
 
 
 
+# .iloc → position-based (use integer positions, like NumPy)
+
+df.iloc[0] # 1st row
+
+df.iloc[0 , 2] # 1st eow and col at position 2
+
+df.iloc[0:5 , 0:3] # first 5 rows and first 3 col
+
+df.iloc[-1] # last row
+
+
