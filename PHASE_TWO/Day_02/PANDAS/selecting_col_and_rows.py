81b967b63b7abd3057668e498cb690bbaa78a8e4
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-07-20 22:47:19 +0500

    Update selecting_col_and_rows.py

--- PHASE_TWO/Day_02/PANDAS/selecting_col_and_rows.py
+++ PHASE_TWO/Day_02/PANDAS/selecting_col_and_rows.py
@@ -43,3 +43,7 @@ df.iloc[0:5 , 0:3] # first 5 rows and first 3 col
 df.iloc[-1] # last row
 
 
+#The .loc vs .iloc distinction matters. Remember it this way:
+
+# .loc → label → you use the actual name
+# .iloc → integer → you use the position number
