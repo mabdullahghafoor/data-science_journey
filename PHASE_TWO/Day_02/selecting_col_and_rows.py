8ab01fe3b277df757da2f86712501040874f5a1b
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-07-20 22:43:36 +0500

    Delete selecting_col_and_rows.py

--- PHASE_TWO/Day_02/selecting_col_and_rows.py
+++ PHASE_TWO/Day_02/selecting_col_and_rows.py
@@ -1,8 +1,3 @@
-# Single column → returns a Series
-
-df.Age  # Whe co;l name has space dont use rhis use brackets
-df["Age"]
-
 # Multiple columns → returns a DataFrame
 
 df[["Name" , "Age" , "Survived"]]
