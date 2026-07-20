5e346980a0e58db4a890de3fee0f96162321c930
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-07-20 22:46:06 +0500

    Create first_six_func_fot _data.py

---
+++ PHASE_TWO/Day_02/PANDAS/first_six_func_fot _data.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("titanic.csv")
+
+df.head() # For first 5 rows
+
+df.tail(3) # For lat 3 rows
+ 
+df.shape # No of rows and col
+
+df.info() # for no f col, data types and Non Null values
+
+df.describe() # Summary for each col like mean, std , var
+
+df.columns # for no of col
+
+
