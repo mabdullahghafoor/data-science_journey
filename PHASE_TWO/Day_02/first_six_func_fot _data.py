0c722bf80e3860f715aae309ebaab40e0e34d9e4
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-07-20 22:42:42 +0500

    Delete first_six_func_fot _data.py

--- PHASE_TWO/Day_02/first_six_func_fot _data.py
+++ PHASE_TWO/Day_02/first_six_func_fot _data.py
@@ -1,14 +1,2 @@
- 0   PassengerId  891 non-null    int64  
- 1   Survived     891 non-null    int64  
- 2   Pclass       891 non-null    int64  
- 3   Name         891 non-null    object 
- 4   Sex          891 non-null    object 
- 5   Age          714 non-null    float64  ← 177 missing values
- 6   SibSp        891 non-null    int64  
- 7   Parch        891 non-null    int64  
- 8   Ticket       891 non-null    object 
- 9   Fare         891 non-null    float64
-10   Cabin        204 non-null    object   ← 687 missing — almost empty
-11   Embarked     889 non-null    object   ← 2 missing
 '''
 
