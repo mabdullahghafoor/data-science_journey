f25c7cf78f5d45e59a5c97fc49d34fb6349f41a1
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-07-20 22:47:11 +0500

    Update selecting_col_and_rows.py

--- PHASE_TWO/Day_02/PANDAS/selecting_col_and_rows.py
+++ PHASE_TWO/Day_02/PANDAS/selecting_col_and_rows.py
@@ -20,3 +20,15 @@ df[["Name" , "Age" , "Survived"]]
 # --- SELECTING ROWS: .loc and .iloc ---
 
 
+# .loc  → label-based (use index labels and column names)
+
+df.loc[0]  # 1st row
+
+df.loc[0 , "Name"] # 1st roc and name col
+
+df.loc[0:4 , "Name" : "Age"] # ros 0 - 4 and col name through age [Inclusive]
+
+df.loc[df["Age"] > 30 , "Name"] # Rows where age >30 only name col
+
+
+
