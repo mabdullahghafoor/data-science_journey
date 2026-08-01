14c982cab65013440cf312ef1ba84a419a36c95d
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-08-02 00:09:11 +0500

    Update detecting_missing_values.py

--- PHASE_TWO/Day_03/Data_Cleaning/detecting_missing_values.py
+++ PHASE_TWO/Day_03/Data_Cleaning/detecting_missing_values.py
@@ -9,6 +9,8 @@ df = pd.read_csv(url)
 
 a = df.isnull().sum()
 print(a)
+print()
+# Percentage of Missing Values For More Useful Decision
 
 # Percentage missing — more useful for decisions
 (df.isnull().sum() / len(df) * 100).round(2)
