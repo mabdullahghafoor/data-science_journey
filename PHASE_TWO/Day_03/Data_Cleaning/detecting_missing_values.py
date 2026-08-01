9c8635b36d1838979c2b61798d5766ed6d83fae6
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-08-02 00:09:02 +0500

    Update detecting_missing_values.py

--- PHASE_TWO/Day_03/Data_Cleaning/detecting_missing_values.py
+++ PHASE_TWO/Day_03/Data_Cleaning/detecting_missing_values.py
@@ -7,15 +7,6 @@ df = pd.read_csv(url)
 
 # Count of nulls per column
 
-# Name             0
-# Sex              0
-# Age            177   ← 177 missing
-# SibSp            0
-# Parch            0
-# Ticket           0
-# Fare             0
-# Cabin          687   ← 687 missing (77% of data!)
-# Embarked         2   ← only 2 missing
 
 # Percentage missing — more useful for decisions
 (df.isnull().sum() / len(df) * 100).round(2)
