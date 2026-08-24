315734ebb42d9e57959b750afbc4f474526eea75
mabdullahghafoor <abdullahghafoor119@gmail.com>
2026-08-24 20:44:49 +0500

    Create easy_pr_problems.py

---
+++ PHASE_1/Day_01/easy_pr_problems.py
@@ -0,0 +1,24 @@
+# In this file we will some some easy practice problems of variables and data types
+
+# Q1. Create variables to store your name, age, city, and whether you're a student. Print all of them using a single f-string.
+
+name = "Abdullah"
+age = 21
+city = "Karachi"
+is_student = True
+
+print (f"My name is {name}, I am {age} years old and I live in {city}. It is {is_student} that I am a Student")
+
+
+# Q2. What will this print? Think before running:
+
+x = 10
+y = "10"
+print(x == y)
+print(type(x) == type(y))
+
+# Q3. Convert "3.99" to a float, then multiply it by 3. Print the result.
+
+val = "3.99"
+con = float(val)
+print(con*3)
